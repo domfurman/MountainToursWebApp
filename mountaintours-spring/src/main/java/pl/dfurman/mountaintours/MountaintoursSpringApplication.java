f58d272311826fe36678c5eac1158bc1d520a455
domfurman <s222915@student.uek.krakow.pl>
2024-03-24 21:12:10 +0100

    created directories to store angular and spring apps with basic configuration

---
+++ mountaintours-spring/src/main/java/pl/dfurman/mountaintours/MountaintoursSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.dfurman.mountaintours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountaintoursSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountaintoursSpringApplication.class, args);
+	}
+
+}
