64a889c95d09d115b4d5b0eac4ce5aa46dd35b47
domfurman <s222915@student.uek.krakow.pl>
2024-03-31 19:43:53 +0200

    security and works on user registration

---
+++ mountaintours-spring/src/main/java/pl/dfurman/mountaintours/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package pl.dfurman.mountaintours.registration;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
