4fd1ba054cceabea4ad027ae53075ff107ea3745
Dominik Furman <s222915@student.uek.krakow.pl>
2024-05-20 20:23:28 +0200

    implemented mapy.cz API

--- mountaintours-angular/src/app/components/map/map.component.ts
+++ mountaintours-angular/src/app/components/map/map.component.ts
@@ -10,6 +10,8 @@ import * as L from 'leaflet';
 })
 export class MapComponent {
 
+  API_KEY = "RzwAWVCO0lNed8aeU4gR_nN5zFcmzmuG2EtCvQdCmZM";
+
   ngOnInit(): void {
     this.configMap()
   }
@@ -18,15 +20,43 @@ export class MapComponent {
 
   configMap() {
     //
-    this.map = L.map('map', {zoom: 10}).locate({
-      setView: true,
-      maxZoom: 16,
+
+    this.map = L.map('map').setView([49.8729317, 14.8981184], 16 );
+
+    L.tileLayer(`https://api.mapy.cz/v1/maptiles/basic/256/{z}/{x}/{y}?apikey=${this.API_KEY}`, {
+      minZoom: 0,
+      maxZoom: 19,
+      attribution: '<a href="https://api.mapy.cz/copyright" target="_blank">&copy; Seznam.cz a.s. a další</a>',
+    }).addTo(this.map);
+
+    const LogoControl = L.Control.extend({
+      options: {
+        position: 'bottomleft',
+      },
+
+      onAdd: function (map: L.Map) {
+        const container = L.DomUtil.create('div');
+        const link = L.DomUtil.create('a', '', container);
+
+        link.setAttribute('href', 'http://mapy.cz/');
+        link.setAttribute('target', 'blank');
+        link.innerHTML = '<img src="https://api.mapy.cz/img/api/logo.svg" alt=""/>';
+        L.DomEvent.disableClickPropagation(link);
+
+        return container;
+      },
     });
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
+    new LogoControl().addTo(this.map)
+    // this.map = L.map('map', {zoom: 10}).locate({
+    //   setView: true,
+    //   maxZoom: 16,
+    // });
+    //
+    // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution:
+    //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    // }).addTo(this.map);
   }
 
 }
